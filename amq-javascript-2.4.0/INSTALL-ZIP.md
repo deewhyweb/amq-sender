93b30fa87c56ee336e3ba6c47ca9bdf1c0a79a8c
Philip Hayes <phayes@redhat.com>
2019-08-26 16:13:58 -0400

    Initial commit

---
+++ amq-javascript-2.4.0/INSTALL-ZIP.md
@@ -0,0 +1,10 @@
+# Install rhea from a zip file:  amq-javascript-2.4.0.zip
+
+* Install dependencies: nodejs and nodejs-debug.
+* Add the rhea node_modules dir to NODE_PATH.
+* Linux/Unix example: 
+  $ unzip amq-javascript-2.4.0.zip 
+  $ cd amq-javascript-2.4.0 
+  $ export NODE_PATH=$PWD/node_modules:$NODE_PATH 
+
+See README.md file for additional information.
---
+++ sender.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2015 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const port = process.env['port']? process.env['port'] : 5672;
+const host = process.env['host']? process.env['host'] : '127.0.0.1';
+const msg = process.env['msg']? process.env['msg'] : 'test message';
+const username = process.env['username']? process.env['username'] : 'artemis';
+const password = process.env['password']? process.env['password'] : 'simetraehcapa';
+
+var container = require('rhea');
+container.on('connection_open', function (context) {
+    context.connection.open_sender('examples');
+});
+
+container.on('sendable', function (context) {
+    context.sender.send({msg});
+    context.sender.detach();
+    context.connection.close();
+});
+container.connect({port: port, host: host, username: username, password: password});
